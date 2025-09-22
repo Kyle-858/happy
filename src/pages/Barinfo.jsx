b0090b8b92791aa987189e9643e5597fdb0fc393
Kyle-858 <krazykyle19@gmail.com>
2025-09-22 00:46:06 -0400

    Improved UI + info screen

--- src/pages/Barinfo.jsx
+++ src/pages/Barinfo.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import './Barinfo.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 const Barinfo = () => {
+
+    const [sort, setSort] = useState("")
+    const navigate = useNavigate()
+    const [option, setOption] = useState('all')
+
   return (
     <div>
-      <h4 className="bar-title">Bar Title</h4>
-      <span className="open-hours">Open: 9am - 6pm</span>
-      <span className="happy-hour">Open: 5pm - 6pm</span>
+        <div className="back-btn" onClick={() => navigate(-1)}>
+            <i class="fa-solid fa-arrow-left"></i>
+        </div>
+        <div className="top-text">
+            <h4 className="bar-title">Chili's, Pitt. Mills</h4>
+            <span className="barinfo-distance">7.5 mi</span>
+        </div>
+        <div className="bar-hours">
+            <span className="open-hours">Open 9am - 6pm</span>
+            <span className="happy-hour">HH 5pm - 6pm</span>
+        </div>
+        <div className="drinks-options">
+            <div className={`option ${option === 'all' ? 'selected' : ''}`} onClick={() => setOption('all')}>All</div>
+            <div className={`option ${option === 'beer' ? 'selected' : ''}`} onClick={() => setOption('beer')}>Beer</div>
+            <div className={`option ${option === 'cocktails' ? 'selected' : ''}`} onClick={() => setOption('cocktails')}>Cocktails</div>
+            <div className={`option ${option === 'apps' ? 'selected' : ''}`} onClick={() => setOption('apps')}>Apps</div>
+        </div>
+        <select value={sort} onChange={(e) => setSort(e.target.value)} className='sort-btn'>
+            <option value="rating">Rating</option>
+            <option value="rating">Price</option>
+            <option value="rating">APV</option>
+        </select>
+        <div className="items-list">
+            {new Array(18).fill(0).map((item, index) => (
+            <div className="item" key={index}>
+                <span className="item-name">Name</span>
+                <span className="item-rating">3.5 / 5</span>
+                <div className="item-priceinfo">
+                    <span className="item-price">$9.99</span>
+                    <span className="item-price">HH $5.99</span>
+                </div>
+            </div>))}
+        </div>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import Loading from '../components/Loading.jsx'
 import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import Search from '../components/Search.jsx'
 import map from '../assets/map.jpg'
 import './Home.css'
@@ -11,13 +11,14 @@ const Home = () => {
 
     const [loading, setLoading] = useState(true)
     const [option, setOption] = useState('map')
+    const navigate = useNavigate()
 
     useEffect(() => {
             // Simulate a 2-second loading
             const timer = setTimeout(() => {
                 setLoading(false);
             }, 2000);
-            // Cleanup in case component unmounts early
+            // Cleanup for if component unmounts early
             return () => clearTimeout(timer);
         }, []);
     
@@ -32,18 +33,16 @@ const Home = () => {
             <img src={map} alt="" className="map"/>
             <div className="bottom-modal">
                 <div className="bars-list">
-                    <Link to='/barinfo'>
-                        <div className="bar">
-                            <div className="bar-text-left">
-                                <div className="bar-name">AppleBee's</div>
-                                <div className="bar-distance">7.1m</div>
-                            </div>
-                            <div className="bar-text-right">
-                                <div className="bar-happyhour">HH: 4-6pm</div>
-                            </div>
+                    <Link to='/barinfo' className="bar">
+                        <div className="bar-text-left">
+                            <div className="bar-name">AppleBee's</div>
+                            <div className="bar-distance">7.1m</div>
+                        </div>
+                        <div className="bar-text-right">
+                            <div className="bar-happyhour">HH: 4-6pm</div>
                         </div>
                     </Link>
-                    <div className="bar">
+                    <Link to='/barinfo' className="bar">
                         <div className="bar-text-left">
                             <div className="bar-name">Chili's</div>
                             <div className="bar-distance">8m</div>
@@ -51,8 +50,8 @@ const Home = () => {
                         <div className="bar-text-right">
                             <div className="bar-happyhour">HH: 4-7pm</div>
                         </div>
-                    </div>
-                    <div className="bar">
+                    </Link>
+                    <Link to='/barinfo' className="bar">
                         <div className="bar-text-left">
                             <div className="bar-name">Chili's</div>
                             <div className="bar-distance">8m</div>
@@ -60,7 +59,7 @@ const Home = () => {
                         <div className="bar-text-right">
                             <div className="bar-happyhour">HH: 4-7pm</div>
                         </div>
-                    </div>
+                    </Link>
                 </div>
             </div>
         </>
